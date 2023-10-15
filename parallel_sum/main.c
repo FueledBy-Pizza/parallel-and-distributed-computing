392c60cd95ff09c080ae4013ec56a666697be471
giuliano <giuliano.aiello.97@gmail.com>
2023-10-15 17:20:18 +0200

    Hello world processes

---
+++ parallel_sum/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]){
+    int nproc, curr_proc;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &curr_proc);
+
+    printf("Hello world! Here it's process %d of %d total processes\n", curr_proc, nproc);
+
+    MPI_Finalize();
+    return 0;
+}
